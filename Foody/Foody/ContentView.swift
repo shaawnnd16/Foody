a607b1725141a5d069fdd8c73aa0159ead40b0ae
SHAWN <128967803+shaawnnd16@users.noreply.github.com>
2024-09-18 02:17:55 +1000

    update

---
+++ Foody/Foody/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Foody
+//
+//  Created by Shawn De Alwis on 18/9/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Foody/Foody/FoodyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodyApp.swift
+//  Foody
+//
+//  Created by Shawn De Alwis on 18/9/2024.
+//
+
+import SwiftUI
+
+@main
+struct FoodyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
